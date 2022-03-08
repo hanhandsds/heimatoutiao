4515853e89e318f0fd6379f623f40ec85e43c562
han <itheima@itcast.cn>
2022-03-08 20:56:20 +0800

    token

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 import './styles/index.less'
-
+import 'amfe-flexible'
 Vue.use(Vant)
 // 加载全局样式
 
--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,20 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const TOKEN_KEY = 'TOUTIAO_USER'
 export default new Vuex.Store({
   state: {
+    // 一个对象，存储当前登录的用户信息(token等数据)
+    uesr: window.localStorage.getItem(TOKEN_KEY)
   },
   getters: {
   },
   mutations: {
+    setUser (state, data) {
+      state.user = data
+      // 为了防止刷新丢失，我们需要把数据备份到本地存储
+      window.localStorage.setItem(TOKEN_KEY, JSON.stringify(state.user))
+    }
   },
   actions: {
   },
